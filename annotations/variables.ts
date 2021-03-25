c2c7b892b2c19fe53a972e2789f1fdfb32356bdb
Riley Butterfield <rileybutterfield1@gmail.com>
2021-03-25 19:04:26 -0400

    type annotations

---
+++ annotations/variables.ts
@@ -0,0 +1,24 @@
+let apples: number = 5
+let speed: string = "fast"
+let hasName: boolean = true
+let nothingMuch: null = null
+let nothing: undefined = undefined
+let now: Date = new Date()
+//Array
+let colors: string[] = ["red", "green", "blue"]
+let myNumbers: number[] = [1, 2 , 3]
+let truths: boolean[] = [true, true, true]
+//Class
+class Car {
+
+}
+let car: Car = new Car()
+//Object Literal
+let point: {x: number; y: number} = {
+  x: 10,
+  y: 20
+}
+//Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i)
+}
